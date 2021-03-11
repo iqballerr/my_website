86ab3c5b36a9e97f25e991d5ce350e9d6fa4b114
iqballerr <ilodin@uci.edu>
2021-03-10 20:39:57 -0800

    update console

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ import About from './pages/About';
 import Links from './pages/Links';
 import Projects from './pages/Projects';
 
+console.log(`%c
+${' '.repeat(33)}
+${' '.repeat(7)}Made by Iqbal Lodin${' '.repeat(7)}
+${' '.repeat(33)}
+${' '.repeat(2)}Want to see the source code?${' '.repeat(3)}
+${' '.repeat(33)}\n`,
+    'background: #007aff; color: white'
+);
+console.log('\nhttps://github.com/iqballerr/my_website\n ')
+
+
 const App = () => {
 	return (
     <Router>
